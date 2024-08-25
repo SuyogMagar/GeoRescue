5a2ea3306af0d9d7b2aaa37920f5d042bcbf135a
Akshat Dwivedi <111682904+akshatdwivedi24@users.noreply.github.com>
2024-08-25 18:50:53 +0530

    Add files via upload

---
+++ BackEnd/src/main/java/com/finalproject/georescue/GeoRescueApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.georescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoRescueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoRescueApplication.class, args);
+    }
+
+}

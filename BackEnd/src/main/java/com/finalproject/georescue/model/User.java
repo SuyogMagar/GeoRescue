b32851f0428ff15a19e09db60dfb91c65025fa34
21BCS7587_Akshat Dwivedi <akshatdwivedi2000@gmail.com>
2024-10-17 18:34:21 +0530

    Updated User.java File

--- BackEnd/src/main/java/com/finalproject/georescue/model/User.java
+++ BackEnd/src/main/java/com/finalproject/georescue/model/User.java
@@ -1,14 +1,18 @@
 package com.finalproject.georescue.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "users")
+@Document(collection = "Users")
 public class User {
 
     @Id
     private String id;
+
+    @Indexed(unique = true)
     private String username;
+    @Indexed(unique = true)
     private String email;
     private String password;
 
@@ -46,4 +50,3 @@ public class User {
         this.password = password;
     }
 }
-

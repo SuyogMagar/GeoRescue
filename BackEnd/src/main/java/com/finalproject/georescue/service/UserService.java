3b1b39e7b2947e164827b2a63102bb313164f25d
21BCS7587_Akshat Dwivedi <akshatdwivedi2000@gmail.com>
2024-10-17 18:35:24 +0530

    Updated UserService.java File

--- BackEnd/src/main/java/com/finalproject/georescue/service/UserService.java
+++ BackEnd/src/main/java/com/finalproject/georescue/service/UserService.java
@@ -1,25 +1,33 @@
 package com.finalproject.georescue.service;
 
-
-
 import com.finalproject.georescue.model.User;
 import com.finalproject.georescue.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder; // Use PasswordEncoder instead of BCryptPasswordEncoder
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder; // Change type to PasswordEncoder
+
     @Autowired
-    private UserRepository userRepository;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder; // Spring will inject the BCryptPasswordEncoder here
+    }
 
     public User registerUser(User user) {
         // Check if the email is already registered
         if (userRepository.findByEmail(user.getEmail()) != null) {
             throw new RuntimeException("Email already registered");
         }
-        // Save new user
+
+        // Hash the password before saving the user
+        user.setPassword(passwordEncoder.encode(user.getPassword())); // Use the injected PasswordEncoder
+
+        // Save the new user with hashed password
         return userRepository.save(user);
     }
 }

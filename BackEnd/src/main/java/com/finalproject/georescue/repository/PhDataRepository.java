4c410df6979b19f5f94b82e69118ed838f82c9e1
21BCS7587_Akshat Dwivedi <akshatdwivedi2000@gmail.com>
2024-10-24 16:52:59 +0530

    File Updated Successfully

--- BackEnd/src/main/java/com/finalproject/georescue/repository/PhDataRepository.java
+++ BackEnd/src/main/java/com/finalproject/georescue/repository/PhDataRepository.java
@@ -1,2 +1,8 @@
-package com.finalproject.georescue.repository;public interface phDataRepository {
+package com.finalproject.georescue.repository;
+
+import com.finalproject.georescue.model.PhData;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PhDataRepository extends MongoRepository<PhData, String> {
+    // You can add custom query methods here if needed
 }

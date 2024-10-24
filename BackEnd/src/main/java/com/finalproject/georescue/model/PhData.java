5f6bbad24d26d7b05a0d35b24cb252910c0525de
21BCS7587_Akshat Dwivedi <akshatdwivedi2000@gmail.com>
2024-10-24 16:52:02 +0530

    File Updated successfully

--- BackEnd/src/main/java/com/finalproject/georescue/model/PhData.java
+++ BackEnd/src/main/java/com/finalproject/georescue/model/PhData.java
@@ -1,2 +1,46 @@
-package com.finalproject.georescue.model;public class phData {
+package com.finalproject.georescue.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "phData")
+public class PhData {
+    @Id
+    private String id;
+    private float value;
+    private LocalDateTime timestamp;
+
+    // Constructors, getters, and setters
+    public PhData() {}
+
+    public PhData(float value, LocalDateTime timestamp) {
+        this.value = value;
+        this.timestamp = timestamp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }

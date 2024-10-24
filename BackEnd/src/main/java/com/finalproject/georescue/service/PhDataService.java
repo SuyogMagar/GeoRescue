b18f3da809469af326f90a0413b97665bf6098da
21BCS7587_Akshat Dwivedi <akshatdwivedi2000@gmail.com>
2024-10-24 16:54:00 +0530

    File Added Successfully

--- BackEnd/src/main/java/com/finalproject/georescue/service/PhDataService.java
+++ BackEnd/src/main/java/com/finalproject/georescue/service/PhDataService.java
@@ -1,2 +1,17 @@
-package com.finalproject.georescue.service;public class PhDataService {
+package com.finalproject.georescue.service;
+
+import com.finalproject.georescue.model.PhData;
+import com.finalproject.georescue.repository.PhDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PhDataService {
+
+    @Autowired
+    private PhDataRepository phDataRepository;
+
+    public PhData savePhData(PhData phData) {
+        return phDataRepository.save(phData);
+    }
 }
